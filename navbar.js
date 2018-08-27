22d1520c31c3aac6fefad44cbd4e01b8ca65ad1a
GoutamKulabhi <goutam.kulabhi@gmail.com>
2018-08-27 22:58:43 +0530

    initial-commit

---
+++ navbar.js
@@ -0,0 +1,42 @@
+$(document).ready(()=>{
+
+	$(".test1").mouseenter(function(){
+		$(this).addClass("color");
+	});
+
+	$(".test1").mouseleave(function(){
+		$(this).removeClass("color");
+	});
+
+	$(".test2").mouseenter(function(){
+		$(this).addClass("color");
+	});
+
+	$(".test2").mouseleave(function(){
+		$(this).removeClass("color");
+	});
+
+	$(".test3").mouseenter(function(){
+		$(this).addClass("color");
+	});
+
+	$(".test3").mouseleave(function(){
+		$(this).removeClass("color");
+	});
+
+	$(".test4").mouseenter(function(){
+		$(this).addClass("color");
+	});
+
+	$(".test4").mouseleave(function(){
+		$(this).removeClass("color");
+	});
+
+	$(".test5").mouseenter(function(){
+		$(this).addClass("color");
+	});
+
+	$(".test5").mouseleave(function(){
+		$(this).removeClass("color");
+	});
+});
